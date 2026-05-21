4630b94e1fb6c0f3a9a6e925434f857bad9f0713
dextter1913 <dextter1913@gmail.com>
2026-05-21 05:28:54 -0500

    feat: adding invoice item

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dextter1913/composition
+
+go 1.26.2
--- pkg/invoice/invoice.go
+++ pkg/invoice/invoice.go
@@ -1,9 +1,11 @@
 package invoice
 
+import "github.com/dextter1913/composition/pkg/customer"
+
 type Invoice struct {
 	country string
 	city string
 	total float64
-	
+	client customer.Customer
 }
 
