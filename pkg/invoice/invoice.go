9ec55ee927b54fc3668dc7cc1807b0e946cf88c1
dextter1913 <dextter1913@gmail.com>
2026-05-29 07:15:59 -0500

    feat: add constructor and logic in invoice

--- pkg/invoice/invoice.go
+++ pkg/invoice/invoice.go
@@ -1,11 +1,30 @@
 package invoice
 
-import "github.com/dextter1913/composition/pkg/customer"
+import (
+	"github.com/dextter1913/composition/pkg/customer"
+	"github.com/dextter1913/composition/pkg/invoiceitem"
+)
 
 type Invoice struct {
 	country string
-	city string
-	total float64
-	client customer.Customer
+	city    string
+	total   float64
+	client  customer.Customer
+	items   []invoiceitem.Item // con los corchetes se indica relacion de uno a muchos
 }
 
+func New(country, city string, client customer.Customer, items []invoiceitem.Item) Invoice {
+	return Invoice{
+		country: country,
+		city:    city,
+		client:  client,
+		items:   items,
+	}
+}
+
+// Set Total is the setter of Invoice.total
+func (i *Invoice) SetTotal() {
+	for _, item := range i.items {
+		i.total += item.Value()
+	}
+}
